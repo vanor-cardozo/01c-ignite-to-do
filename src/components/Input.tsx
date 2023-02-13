9f8a11f167a182663b84c739a14665894e707840
vanor-cardozo <vanor.fc@gmail.com>
2023-02-13 14:34:37 -0300

    feat: Configuração para CSS e Ícones

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import styles from './Input.module.css';
 
 import {v4 as uuidv4} from 'uuid';
-import { TaskList } from './TaskList';
+
+import {PlusCircle, ClipboardText, Trash} from 'phosphor-react'
 
 export function Input() {
     const [tasks, setTasks] = React.useState([]);
@@ -43,49 +44,55 @@ export function Input() {
 
     return (
         <div className={styles.container}>
-            <form>
-                <label>
-                    <input
-                        type="text"
-                        placeholder="Adicione uma nova tarefa"
-                        onChange={handleChangeTask}
-                        value={newTask.name}
-                    />
-                </label>
-                {!!newTask.name ?
-                    <button type='submit' onClick={handleCreateTask}>Criar</button>
-                :
-                    <button disabled>Criar</button>
-                }
-            </form>
-            <div>
-                <div>
-                    <div>
-                        <strong>Tarefas criadas</strong>
-                        <p>{countTasks()}</p>
+            <div className={styles.container2}>
+                <form>
+                    <label>
+                        <input
+                            type="text"
+                            placeholder="Adicione uma nova tarefa"
+                            onChange={handleChangeTask}
+                            value={newTask.name}
+                        />
+                    </label>
+                    {!!newTask.name ?
+                        <button type='submit' onClick={handleCreateTask}>Criar</button>
+                    :
+                        <button disabled>Criar <PlusCircle size={24}/> </button>
+                    }
+                </form>
+            </div>
+
+                <div className={styles.container3}>
+                    <div className={styles.container4}>
+                        <strong className={styles.counterBlue}>Tarefas criadas</strong>
+                        <strong className={styles.bgCounter}>{countTasks()}</strong>
                     </div>
-                    <div>
-                        <strong>Concluídas</strong>
-                        <p>{countTasksDone()} de {countTasks()}</p>
+                    <div className={styles.container4}>
+                        <strong className={styles.counterPurple}>Concluídas</strong>
+                        <strong className={styles.bgCounter}>{countTasksDone()} de {countTasks()}</strong>
                     </div>
                 </div>
-                {tasks.map((task)=> (
+                {tasks.length > 0 && tasks.map((task)=> (
                     <div key={task.id} className={styles.taskListItem}>
-                        <>
+                        <label htmlFor={task.id}>
                             <input
                                 type="checkbox"
                                 id={task.id}
                                 name={task.name}
                                 onClick={handleStatusDone}
                             />
-                            <label htmlFor={task.id}>{task.name}</label>
-                        </>
-
-                    <button onClick={()=> handleDelete(task.id)}>deletar</button>
+                            <p>{task.name}</p>
+                        </label>
+                        <Trash onClick={()=> handleDelete(task.id)} size={20}/>
                     </div>
                 ))}
-            </div>
-            
+                {tasks.length === 0 &&
+                    <div className={styles.emptyInfo}>
+                        <ClipboardText size={50} weight="thin"/>
+                        <strong>Você ainda não tem tarefas cadastradas</strong>
+                        <p>Crie tarefas e organize seus itens a fazer</p>
+                    </div>
+                }
         </div>
     )
 }
